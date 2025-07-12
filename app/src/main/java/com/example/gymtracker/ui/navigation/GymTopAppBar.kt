e1f098ba92db77f15f8031e33f8ac21d7e62818f
Toni Cantarella <toni.cantarella@outlook.com>
2025-07-12 13:32:32 +0300

    Added a debounce and animated visibility for topbar on screens not providing it

--- app/src/main/java/com/example/gymtracker/ui/navigation/GymTopAppBar.kt
+++ app/src/main/java/com/example/gymtracker/ui/navigation/GymTopAppBar.kt
@@ -1,13 +1,23 @@
 package com.example.gymtracker.ui.navigation
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavBackStackEntry
+import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -19,12 +29,24 @@ fun GymTopAppBar(
             viewModelStoreOwner = entry,
             initializer = { GymTopAppBarViewModel() }
         )
+        var debouncedShowTopBar by remember { mutableStateOf(true) }
 
-        TopAppBar(
-            title = viewModel.title,
-            navigationIcon = viewModel.navigationIcon,
-            actions = viewModel.actions
-        )
+        LaunchedEffect(viewModel.showTopBar) {
+            delay(50)
+            debouncedShowTopBar = viewModel.showTopBar
+        }
+
+        AnimatedVisibility(
+            visible = debouncedShowTopBar,
+            enter = fadeIn() + expandVertically(),
+            exit = fadeOut() + shrinkVertically()
+        ){
+            TopAppBar(
+                title = viewModel.title,
+                navigationIcon = viewModel.navigationIcon,
+                actions = viewModel.actions
+            )
+        }
     }
 }
 
@@ -41,6 +63,7 @@ fun ProvideTopAppBar(
             initializer = { GymTopAppBarViewModel() },
         )
         LaunchedEffect(title, navigationItem, actions) {
+            viewModel.showTopBar = true
             viewModel.title = title
             viewModel.navigationIcon = navigationItem
             viewModel.actions = actions
