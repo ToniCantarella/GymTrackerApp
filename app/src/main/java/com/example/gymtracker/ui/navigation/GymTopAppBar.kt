2d72c71c010ce72a154e676b198cfe829e118deb
Toni Cantarella <toni.cantarella@outlook.com>
2025-08-16 16:42:55 +0300

    Top bar textfield shows backspace icon instead of a cross to indicate erasing

--- app/src/main/java/com/example/gymtracker/ui/navigation/GymTopAppBar.kt
+++ app/src/main/java/com/example/gymtracker/ui/navigation/GymTopAppBar.kt
@@ -6,8 +6,6 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -25,6 +23,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
@@ -123,7 +122,7 @@ fun TopBarTextField(
                     onClick = { onValueChange("") }
                 ) {
                     Icon(
-                        imageVector = Icons.Default.Close,
+                        painter = painterResource(id = R.drawable.backspace),
                         contentDescription = stringResource(id = R.string.clear)
                     )
                 }
