3203768a27128a4ce1343b2bf988de97fbe51518
Toni Cantarella <toni.cantarella@outlook.com>
2025-08-17 10:16:03 +0300

    Removed the gradient from topbar

--- app/src/main/java/com/example/gymtracker/ui/navigation/GymTopAppBar.kt
+++ app/src/main/java/com/example/gymtracker/ui/navigation/GymTopAppBar.kt
@@ -1,6 +1,5 @@
 package com.example.gymtracker.ui.navigation
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.RowScope
@@ -10,12 +9,9 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -23,8 +19,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
@@ -57,22 +51,7 @@ fun GymTopAppBar(
             TopAppBar(
                 title = viewModel.title,
                 navigationIcon = viewModel.navigationIcon,
-                actions = viewModel.actions,
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = Color.Transparent,
-                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
-                    navigationIconContentColor = MaterialTheme.colorScheme.onPrimary,
-                    actionIconContentColor = MaterialTheme.colorScheme.onPrimary
-                ),
-                modifier = Modifier
-                    .background(
-                        brush = Brush.verticalGradient(
-                            colors = listOf(
-                                Color(0xFFFF6F2B),
-                                Color(0xFFFFB05B)
-                            )
-                        )
-                    )
+                actions = viewModel.actions
             )
         }
     }
@@ -147,13 +126,6 @@ fun TopBarTextField(
                 }
             }
         },
-        colors = TextFieldDefaults.colors(
-            unfocusedContainerColor = Color.Transparent,
-            unfocusedIndicatorColor = Color.Transparent,
-            focusedTextColor = MaterialTheme.colorScheme.onPrimary,
-            unfocusedTextColor = MaterialTheme.colorScheme.onPrimary,
-            unfocusedPlaceholderColor = MaterialTheme.colorScheme.onPrimary
-        ),
         modifier = modifier.fillMaxWidth()
     )
 }
