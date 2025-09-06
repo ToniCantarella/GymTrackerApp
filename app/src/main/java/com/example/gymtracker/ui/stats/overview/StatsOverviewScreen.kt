a1c400c03f903bfa9927ea397fe71bec94493e45
Toni Cantarella <toni.cantarella@outlook.com>
2025-09-06 13:32:50 +0300

    Fixed missing imports in StatsOverviewScreen

--- app/src/main/java/com/example/gymtracker/ui/stats/overview/StatsOverviewScreen.kt
+++ app/src/main/java/com/example/gymtracker/ui/stats/overview/StatsOverviewScreen.kt
@@ -1,19 +1,125 @@
 package com.example.gymtracker.ui.stats.overview
 
+import android.content.res.Configuration
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.Popup
+import androidx.compose.ui.zIndex
 import com.example.gymtracker.R
+import com.example.gymtracker.ui.common.WorkoutListItem
 import com.example.gymtracker.ui.entity.WorkoutWithTimestamp
+import com.example.gymtracker.ui.entity.statsoverview.WorkoutSession
 import com.example.gymtracker.ui.navigation.ProvideTopAppBar
+import com.example.gymtracker.ui.theme.GymTrackerTheme
+import com.example.gymtracker.utility.MAX_GYM_WORKOUTS
+import com.kizitonwose.calendar.compose.HorizontalCalendar
+import com.kizitonwose.calendar.compose.rememberCalendarState
+import com.kizitonwose.calendar.core.CalendarDay
+import com.kizitonwose.calendar.core.DayPosition
+import com.kizitonwose.calendar.core.daysOfWeek
+import com.kizitonwose.calendar.core.minusMonths
+import com.kizitonwose.calendar.core.plusMonths
+import ir.ehsannarmani.compose_charts.PieChart
+import ir.ehsannarmani.compose_charts.models.Pie
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import kotlinx.datetime.DayOfWeek
+import kotlinx.datetime.Month
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.atStartOfDayIn
+import kotlinx.datetime.toJavaDayOfWeek
+import kotlinx.datetime.toJavaLocalDate
+import kotlinx.datetime.toJavaYearMonth
+import kotlinx.datetime.toKotlinLocalDate
+import kotlinx.datetime.toKotlinMonth
+import kotlinx.datetime.toKotlinTimeZone
+import kotlinx.datetime.toKotlinYearMonth
+import kotlinx.datetime.todayIn
 import org.koin.androidx.compose.koinViewModel
+import java.time.Duration
 import java.time.Instant
+import java.time.LocalDate
+import java.time.YearMonth
+import java.time.ZoneId
+import java.time.format.TextStyle
+import java.util.Locale
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
+import kotlin.time.toJavaInstant
 
 @Composable
 fun StatsOverviewScreen(
@@ -43,7 +149,7 @@ fun StatsOverviewScreen(
         }
     )
 
-    /*StatsOverviewScreen(
+    StatsOverviewScreen(
         loading = uiState.loading,
         gymWorkouts = uiState.gymWorkouts,
         cardioWorkouts = uiState.cardioWorkouts,
@@ -54,10 +160,9 @@ fun StatsOverviewScreen(
         onSessionNavigate = onSessionNavigate,
         onAddSessionNavigate = onAddSessionNavigate,
         onWorkoutStatsNavigate = onWorkoutStatsNavigate
-    )*/
+    )
 }
 
-/*
 @Composable
 private fun StatsOverviewScreen(
     loading: Boolean,
@@ -541,9 +646,7 @@ private fun StatCalendar(
                         )
 
                         if (sessionsForDialog.size > 1 && index != sessionsForDialog.lastIndex) {
-                            HorizontalDivider(
-                                color = MaterialTheme.colorScheme.outline
-                            )
+                            HorizontalDivider()
                         }
                     }
                 }
@@ -1043,4 +1146,3 @@ private fun StatsOverviewPreviewDark() {
         }
     }
 }
-*/
