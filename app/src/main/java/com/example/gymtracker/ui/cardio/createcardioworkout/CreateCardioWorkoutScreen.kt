6f52c28f229aac45d56e3dde1195436baf194544
Toni Cantarella <toni.cantarella@outlook.com>
2025-09-11 21:02:33 +0300

    Top bar, FAB and scaffold to create cardio workout screen

--- app/src/main/java/com/example/gymtracker/ui/cardio/createcardioworkout/CreateCardioWorkoutScreen.kt
+++ app/src/main/java/com/example/gymtracker/ui/cardio/createcardioworkout/CreateCardioWorkoutScreen.kt
@@ -4,19 +4,34 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.zIndex
+import com.example.gymtracker.R
 import com.example.gymtracker.ui.cardio.common.CardioContent
+import com.example.gymtracker.ui.common.TopBarTextField
 import com.example.gymtracker.ui.theme.GymTrackerTheme
 import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateCardioWorkoutScreen(
     onNavigateBack: () -> Unit,
@@ -39,17 +54,62 @@ fun CreateCardioWorkoutScreen(
         }
     }
 
-    Box {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .zIndex(1f)
-                .pointerInput(Unit) {}
-                .background(
-                    MaterialTheme.colorScheme.background.copy(alpha = .5f)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    TopBarTextField(
+                        value = uiState.name,
+                        onValueChange = viewModel::onChangeName
+                    )
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            onNavigateBack()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = stringResource(R.string.back)
+                        )
+                    }
+                }
+            )
+        },
+        floatingActionButton = {
+            val enabled = uiState.name.isNotEmpty()
+            FloatingActionButton(
+                onClick = {
+                    if (enabled){
+                        releaseNavigationGuard()
+                        viewModel.onSavePressed { onNavigateBack() }
+                    }
+                },
+                containerColor = if (enabled) MaterialTheme.colorScheme.primary else Color.Gray,
+                contentColor = if (enabled) Color.White else Color.Black.copy(alpha = 0.5f)
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.save),
+                    contentDescription = stringResource(id = R.string.save)
                 )
-        )
-        CardioContent()
+            }
+        }
+    ) { innerPadding ->
+        Box(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .zIndex(1f)
+                    .pointerInput(Unit) {}
+                    .background(
+                        MaterialTheme.colorScheme.background.copy(alpha = .5f)
+                    )
+            )
+            CardioContent()
+        }
     }
 }
 
