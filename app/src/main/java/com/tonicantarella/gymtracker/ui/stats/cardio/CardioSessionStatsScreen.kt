a8b4632dccc887ab2d470953037866e964f77844
Toni Cantarella <toni.cantarella@outlook.com>
2025-09-23 18:25:06 +0300

    Viewing cardio sessions shows cards instead of the cardio content

--- app/src/main/java/com/tonicantarella/gymtracker/ui/stats/cardio/CardioSessionStatsScreen.kt
+++ app/src/main/java/com/tonicantarella/gymtracker/ui/stats/cardio/CardioSessionStatsScreen.kt
@@ -1,13 +1,18 @@
 package com.tonicantarella.gymtracker.ui.stats.cardio
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -20,13 +25,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.zIndex
 import com.tonicantarella.gymtracker.R
-import com.tonicantarella.gymtracker.ui.cardio.common.CardioContent
 import com.tonicantarella.gymtracker.ui.common.GymScaffold
 import com.tonicantarella.gymtracker.ui.entity.cardio.WorkoutWithMetrics
+import com.tonicantarella.gymtracker.utility.UnitUtil
 import com.tonicantarella.gymtracker.utility.toDateAndTimeString
+import com.tonicantarella.gymtracker.utility.toReadableString
 import org.koin.compose.viewmodel.koinViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -95,13 +102,86 @@ private fun CardioSessionStatsScreen(
                             text = cardio.timestamp?.toDateAndTimeString() ?: "-"
                         )
                     }
-                    CardioContent(
-                        steps = cardio.metrics.steps,
-                        distance = cardio.metrics.distance,
-                        displayDuration = cardio.metrics.duration
-                    )
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium)),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(dimensionResource(id = R.dimen.padding_large))
+                    ) {
+                        SessionExerciseCard(
+                            icon = {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.footprint),
+                                    contentDescription = null
+                                )
+                            },
+                            label = stringResource(id = R.string.steps),
+                            value = {
+                                Text(
+                                    text = "${cardio.metrics.steps} ${stringResource(id = R.string.steps)}"
+                                )
+                            }
+                        )
+                        SessionExerciseCard(
+                            icon = {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.path),
+                                    contentDescription = null
+                                )
+                            },
+                            label = stringResource(id = R.string.distance),
+                            value = {
+                                Text(
+                                    text = "${cardio.metrics.distance} ${stringResource(UnitUtil.distanceUnitStringId)}"
+                                )
+                            }
+                        )
+                        SessionExerciseCard(
+                            icon = {
+                                Icon(
+                                    painter = painterResource(id = R.drawable.timer),
+                                    contentDescription = null
+                                )
+                            },
+                            label = stringResource(id = R.string.time),
+                            value = {
+                                Text(
+                                    text = cardio.metrics.duration?.toReadableString() ?: ""
+                                )
+                            }
+                        )
+                    }
                 }
             }
+        }
+    }
+}
+
+@Composable
+fun SessionExerciseCard(
+    icon: @Composable () -> Unit,
+    label: String,
+    value: @Composable () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = dimensionResource(id = R.dimen.card_elevation)
+        ),
+        modifier = modifier
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_large))
+        ) {
+            icon()
+            Spacer(modifier = Modifier.size(dimensionResource(id = R.dimen.padding_medium)))
+            Text(
+                text = label
+            )
+            Spacer(modifier = Modifier.weight(1f))
+            value()
         }
     }
 }
